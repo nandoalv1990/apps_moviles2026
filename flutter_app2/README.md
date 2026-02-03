c106b1af349bb9d37fce726e8bd6fa42a467765a
nandoalv1990 <nandoalv_1990@hotmail.com>
2026-02-03 16:34:28 -0600

    update

--- flutter_app2/README.md
+++ flutter_app2/README.md
@@ -1,3 +1,22 @@
-# flutter_app2
+## App flutter 2:
+  
 
-A new Flutter project.
+
+
+
+
+
+
+  lib/
+ ├─ models/
+ │   └─ movie_model.dart
+ ├─ services/
+ │   ├─ movie_service.dart
+ │   └─ storage_service.dart
+ ├─ screens/
+ │   ├─ movie_list_screen.dart
+ │   ├─ add_movie_screen.dart
+ │   └─ movie_detail_screen.dart
+ └─ widgets/
+     ├─ movie_card.dart
+     └─ rating_stars.dart
---
+++ flutter_app2/lib/screens/movie_list_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MovieListScreen extends StatelessWidget {
+  const MovieListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('MovieRate'),),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context, 
+            MaterialPageRoute(
+              builder: (_) = AddMovieScreen()
+            ),
+          );
+        },
+      ),
+      body: StreamBuilder<List><Movie>(
+        stream: , 
+        builder: (context,sanpshot){
+
+        },
+      ),
+    );
+  }
+}
