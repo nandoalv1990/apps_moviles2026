9f9443e7b7f6b5a2c055a922850d4a6dadf691f5
nandoalvwashere1990 <nandoalv_1990@hotmail.com>
2026-02-23 22:19:30 -0600

    update

--- flutter_app_firebaseauth/android/app/build.gradle.kts
+++ flutter_app_firebaseauth/android/app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
+    // 
+    id("com.google.gms.google-services") 
 }
 
 android {
@@ -20,16 +20,14 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
+    // Modificado
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.firebase_login"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
+    applicationId = "com.example.firebase_login"
+    minSdk = flutter.minSdkVersion
+    targetSdk = flutter.targetSdkVersion
+    versionCode = flutter.versionCode
+    versionName = flutter.versionName
+}
 
     buildTypes {
         release {
@@ -42,4 +40,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// Añadido según doc de FireBase
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-auth-ktx")
 }
--- flutter_app_firebaseauth/android/build.gradle.kts
+++ flutter_app_firebaseauth/android/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 allprojects {
@@ -18,9 +18,6 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
