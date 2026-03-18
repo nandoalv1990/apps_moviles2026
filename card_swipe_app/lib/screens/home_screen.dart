0ce3a6441d04770cc29f8807b7520fbda3dc5684
nandoalv1990 <nandoalv1990@gmail.com>
2026-03-18 14:24:13 -0600

    Funcionalidad completa

--- card_swipe_app/lib/screens/home_screen.dart
+++ card_swipe_app/lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:card_swipe_app/widgets/swipe_card.dart';
 import 'package:flutter/material.dart';
 import '../models/card_item.dart';
 
@@ -9,15 +10,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<CardItem> cards = [
-    CardItem(name: 'Ana', image: 'image', description: 'description'),
-    CardItem(name: 'Jane', image: 'image', description: 'description'),
-    CardItem(name: 'Maria', image: 'image', description: 'description'),
-  ];
 
+  List<CardItem> cards = [
+    CardItem(name: 'Ana', image: 'https://picsum.photos/400/600?1', description: 'description'),
+    CardItem(name: 'Jane', image: 'https://picsum.photos/400/600?2', description: 'description'),
+    CardItem(name: 'Maria', image: 'https://picsum.photos/400/600?3', description: 'description'),
+  ];
   
+  void refresh() {
+  Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(builder: (_) => const HomeScreen()),
+  );
+}
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dinter'),
+        actions: [
+          IconButton(
+            onPressed: refresh, 
+            icon: const Icon(Icons.refresh))
+        ],
+      ),
+      body: Center(
+        child: cards.isEmpty? const Text('No Cards') : 
+          Dismissible(
+            key: ValueKey(cards.last.name), 
+            direction: DismissDirection.horizontal, 
+            onDismissed: (direction) {
+              setState(() {
+                cards.removeLast();
+              });
+            },
+            child: SizedBox(
+              width: 320,
+              height: 500,
+              child: SwipeCard(item: cards.last),
+            ),
+        ),
+      ),
+    );
   }
 }
